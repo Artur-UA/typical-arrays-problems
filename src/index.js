94bc8d0a38f9c693676c41ca685fc917194e6461
Artur-UA <kovalskartur@yahoo.com>
2021-03-15 00:10:31 +0200

    feat : add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0 ) {
+    return Math.min(...array)
+  } 
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return Math.max(...array)
+}
+return 0;
 }
 
 exports.avg = function avg (array) {
+  if (typeof array !== 'undefined' && array.length > 0) {
+    let sum = array.reduce((a, b) => a + b, 0);
+    let result = sum / array.length;
+    return result
+  } 
   return 0;
 }
